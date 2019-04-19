35db00311746491ee2a6adc58ed333531d64ce0e
nad-1 <49733103+nad-1@users.noreply.github.com>
2019-04-19 09:46:21 -0700

    Update osproject.cpp

--- osproject.cpp
+++ osproject.cpp
@@ -1,6 +1,8 @@
  //There are 5 processes and 3 resource types, resource A with 10 instances, B with 5 instances and C with 7 instances.
  // Find whether the system is in safe state or not?
 #include<math.h>
+#include<pthread.h>
+#include<stdlib.h>
 #include<string.h>
 #include <stdio.h> 
 #include<conio.h>
